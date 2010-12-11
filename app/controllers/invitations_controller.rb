349a948d0b1ba6b61b22ae6fd0597f015d62434f
Mostafa R. Arafa <ragab.mostafa@gmail.com>
2010-12-11 13:19:59 +0200

    add locales support

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -12,9 +12,9 @@ class InvitationsController < InvitationApplicationController
       respond_to do |format|
         format.html do
           if current_user.invitations_expiration and current_user.invitations_expiration <= Date.today
-            msg = "Sorry, Your invitations has been expired."
+            msg = t("invitation_system.invitation.messages.expired")
           else
-            msg = "Sorry, You can't invite more users." 
+            msg = t("invitation_system.invitation.messages.more_users")
           end          
           flash[:alert] = msg
           redirect_to user_invitations_path(current_user.id)
@@ -31,24 +31,24 @@ class InvitationsController < InvitationApplicationController
     emails.delete_if { |key, value| value.blank? }
     
     msg = []
-    msg << "Please add at least one valid email address" if emails.empty?
-    msg << "Can't send invitation more than your maximum limit" if emails.length > @user.current_invitations_count
-    msg << "Please type the sender name" if params[:inviter_name].blank?
-    msg << "Please type the invitation message" if params[:body].blank?
+    msg << t("invitation_system.invitation.messages.emails_empty") if emails.empty?
+    msg << t("invitation_system.invitation.messages.maximum_limit") if emails.length > @user.current_invitations_count
+    msg << t("invitation_system.invitation.messages.sender_name") if params[:inviter_name].blank?
+    msg << t("invitation_system.invitation.messages.invitation_message") if params[:body].blank?
     
     emails.each do |key, email|
       if !(email =~ /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i)
-        msg << "Email (#{email}) is not valid email address."
+        msg << t("invitation_system.invitation.messages.not_valid_email", :email=>email)
       elsif User.exists?(['email = ?', email])
-        msg << "The email (#{email}) already exists"
+        msg << t("invitation_system.invitation.messages.email_exists", :email=>email)
       elsif user_invitation = Invitation.find_by_invited_email(email)
         case user_invitation.invitation_status
           when InvitationStatus.waiting
-            msg << "An invitation has been sent to the mail (#{email}), and waiting for reply."        
+            msg << t("invitation_system.invitation.messages.waiting_for_reply", :email=>email)
           when InvitationStatus.expired
-            msg << "An invitation has been sent to the mail (#{email}), and the invitation was expired."
+            msg << t("invitation_system.invitation.messages.invitation_expired", :email=>email)
           when InvitationStatus.accepted
-            msg << "An invitation has been sent to the mail (#{email}), and the invitation was accepted."
+            msg << t("invitation_system.invitation.messages.invitation_accepted", :email=>email)
         end
       end      
     end
@@ -73,7 +73,7 @@ class InvitationsController < InvitationApplicationController
         invitations_list << invitation if !emails_list.include?(email)
         emails_list << email
       else
-        msg << "Email (#{email}) is not valid."
+        msg << t("invitation_system.invitation.messages.not_valid_email", :email=>email)
       end
     end
     
@@ -98,8 +98,8 @@ class InvitationsController < InvitationApplicationController
       
       #Updating current_user with the new number of invitations
       current_user.invitations_count = @user.current_invitations_count
-      
-      flash[:success] = "Invitation has been sent to :<br/>" + emails_list.join("<br/>")
+	  
+      flash[:success] = t("invitation_system.invitation.messages.invitation_sent", :emails=>emails_list.join("<br/>"))
       redirect_to :action => 'index'
       
     rescue => e
@@ -113,11 +113,11 @@ class InvitationsController < InvitationApplicationController
     
     if invitation = Invitation.find(params[:id])
       if User.exists?(['email = ?', invitation.invited_email])
-        msg << "The email (#{email}) already exists"
+        msg << t("invitation_system.invitation.messages.email_exists", :email=>email)
       else
         case invitation.invitation_status
           when InvitationStatus.accepted
-            msg << "An invitation has been sent to the mail (#{email}), and the invitation was accepted."
+            msg << t("invitation_system.invitation.messages.invitation_accepted", :email=>email)
         end
       end 
       
@@ -132,13 +132,13 @@ class InvitationsController < InvitationApplicationController
       begin
         invitation.save!
         InvitationMailer.deliver_invitation(invitation , request.host_with_port)
-        flash[:success] = "The invitation has beed sent to (#{invitation.invited_email}) successfully"
+        flash[:success] = t("invitation_system.invitation.messages.invitation_sent", :emails=>invitation.invited_email)
       rescue => e
         flash[:alert] = e.message
       end    
     
     else
-      flash[:alert] = "Can't find the invitation"
+      flash[:alert] = t("invitation_system.invitation.messages.invitation_not_found")
     end    
     redirect_to user_invitations_path(current_user)
   end
@@ -148,7 +148,7 @@ class InvitationsController < InvitationApplicationController
     if invitation && invitation.invitation_status == InvitationStatus.waiting
       redirect_to new_user_path + "?invitation_key=#{params[:invitation_key]}"
     else
-      flash[:alert] = "This invitation is not valid."
+      flash[:alert] = t("invitation_system.invitation.messages.invitation_not_valid")
       redirect_to root_path
     end
   end
@@ -158,7 +158,7 @@ class InvitationsController < InvitationApplicationController
   
   def is_invitations_enabled?
     if InvitationSetting.registration_type != RegistrationType.registration_with_invitation
-      flash[:alert] = "Sorry, The invitation system has been disabled by the administartor"
+      flash[:alert] = t("invitation_system.invitation.messages.invitation_system_id_disabled")
 	  redirect_to user_invitations_path(current_user)
       return false
     end
